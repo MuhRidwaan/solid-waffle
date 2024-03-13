83f29fac9142fe7ac12c40bd02181669b9c5920b
MuhRidwaan <mridwan07072002@gmail.com>
2024-03-13 22:41:27 +0700

    aplikasi musabaqoh

---
+++ CekLogin.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+require 'koneksi.php';
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $sql_check = "SELECT username, password FROM user WHERE username=? AND password=? LIMIT 1";
+
+    $check_log = $dbconnect->prepare($sql_check);
+    $check_log->bind_param('ss', $username, $password);
+
+    $check_log->execute();
+    $check_log->store_result();
+
+    if ($check_log->num_rows == 1) {
+        // Bind hasil query ke variabel
+        $check_log->bind_result($returned_username, $returned_password);
+    
+        while ($check_log->fetch()) {
+            $_SESSION['user_login'] = $returned_username;
+        }
+    
+        $check_log->close();
+    
+        header('location: index.php?not=1');
+        exit();
+    } else {
+        header('location: login.php?note=1');
+        exit();
+    }
+} else {
+    header('location: login.php?note=1');
+    exit();
+}
+?>
